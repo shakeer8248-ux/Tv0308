a03bfc64e907e0de3c9b45824cf535b8fb1d7dcd
shakeer8248-ux <shakeer8248@gmail.com>
2026-04-29 20:36:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,21 @@ const linkMap = new Map(); // Stores { "1234": "https://terabox.com/..." }
 
 // Mobile sends the TeraBox link here
 app.post('/send-link', (req, res) => {
+    let url = req.body.url;
+
+    // Automatically convert Google Drive links to Direct Stream links
+    if (url.includes('drive.google.com')) {
+        const fileId = url.split('/d/')[1]?.split('/')[0];
+        if (fileId) {
+            url = `https://drive.google.com/uc?export=download&id=${fileId}`;
+        }
+    }
+
     const code = Math.floor(1000 + Math.random() * 9000).toString();
-    linkMap.set(code, req.body.url);
+    linkMap.set(code, url);
     res.json({ code: code });
 });
+
 
 // TV asks for the link using the code
 app.get('/get-link/:code', (req, res) => {
